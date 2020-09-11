2ca75f18c6ee1c392f283e655d0e617117c47629
Abitha <abithraok@gmail.com>
2020-09-11 19:10:04 +0530

    Updates

---
+++ precache-manifest.399a5be41d0bcd8200a1d26626670ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392556b14325c7308949127654b23497",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f0a63ee21e7ed49671db",
+    "url": "/react-app/static/css/main.17367fa6.chunk.css"
+  },
+  {
+    "revision": "3c9fa98192e0c6c7cdd9",
+    "url": "/react-app/static/js/2.51927e41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.51927e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a63ee21e7ed49671db",
+    "url": "/react-app/static/js/main.f4d259ed.chunk.js"
+  },
+  {
+    "revision": "31483861b8fa50fc16a2",
+    "url": "/react-app/static/js/runtime-main.8a930b4d.js"
+  }
+]);
